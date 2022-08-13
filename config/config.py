2a271a06828886c0f1afa9c1ba1bdfe3d7729369
imzaynking <109244346+imzaynking@users.noreply.github.com>
2022-08-13 10:02:32 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -159,7 +159,7 @@ autoclean = []
 
 
 # Images
-START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/e17f42bc195635b668d6d.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/e4f889bdfa3a046809390.mp4")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
